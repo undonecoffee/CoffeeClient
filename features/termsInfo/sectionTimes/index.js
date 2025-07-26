1c148e9cbdd460dd07543a64fea42618475b38c3
undonecoffee <cameronwilcox101@gmail.com>
2025-07-26 01:45:25 -0500

    section times start time bug fix

--- features/termsInfo/sectionTimes/index.js
+++ features/termsInfo/sectionTimes/index.js
@@ -8,8 +8,11 @@ let waiting = false
 let gateBlown = false
 let sectionTime = Date.now()
 register("chat", (name, action, object, completed, total, event) => {
+    if (section == 0) {
+        sectionTime = Date.now()
+        section = 1
+    }
     if (completed !== total) return
-    // ChatLib.chat(`(${completed}/${total})`)
     if (!gateBlown) waiting = true
     else newSection()
 }).setCriteria(/^(.+) (activated|completed) a (terminal|device|lever)! \((\d)\/(\d)\)/)
@@ -23,7 +26,6 @@ function newSection() {
     waiting = false
     gateBlown = false
     section++
-    // ChatLib.chat("now in section " + section)
     if (section == 4) gateBlown = true
     guis.sectionTimes.toggled = true
     guis.sectionTimes.name = `&3${((Date.now() - sectionTime) / 1000).toFixed(2)}s`
@@ -32,6 +34,7 @@ function newSection() {
 }
 
 register("chat", () => {
-    if (section == 0) section = 1
+    if (section !== 0) return
     sectionTime = Date.now()
+    section = 1
 }).setCriteria(/^\[BOSS\] Goldor: Who dares trespass into my domain?/)
