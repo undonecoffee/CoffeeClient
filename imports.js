113e87a5b4351c7fabca54f7c378c997c2163d78
undonecoffee <cameronwilcox101@gmail.com>
2025-07-25 22:50:51 -0500

    imported terms

--- imports.js
+++ imports.js
@@ -1,6 +1,6 @@
 // configs:
 import "./features/misc/config/index"
-// import "./features/termsInfo/config/index"
+import "./features/termsInfo/config/index"
 // import "./features/splits/config/index"
 // import "./features/autoP3/config/index"
 // import "./features/autoClear/config/index"
