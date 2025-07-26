fd7a93542493f542accf467d820f5cacb5067829
undonecoffee <cameronwilcox101@gmail.com>
2025-07-25 22:39:50 -0500

    testttt

--- imports.js
+++ imports.js
@@ -1,27 +1,15 @@
 // configs:
 import "./features/misc/config/index"
-import "./features/termsInfo/config/index"
-import "./features/splits/config/index"
+// import "./features/termsInfo/config/index"
+// import "./features/splits/config/index"
 // import "./features/autoP3/config/index"
 // import "./features/autoClear/config/index"
 // import "./features/autoBoss/config/index"
 
 // misc:
-import "./features/misc/autoBless/index"
+// import "./features/misc/autoBless/index"
 import "./features/misc/autoRequeue/index"
 // import "./features/misc/chatCleaner/index"
 import "./features/misc/maskTimer/index"
-// import "./features/misc/mobHighlight/index"
-import "./features/misc/relicUtils/index"
+import "./features/misc/mobHighlight/index"
 import "./features/misc/sheepHider/index"
-// termsInfo:
-import "./features/termsInfo/movementTimer/index"
-import "./features/termsInfo/positionalAlerts/index"
-import "./features/termsInfo/sectionTimes/index"
-import "./features/termsInfo/termInfo/index"
-import "./features/termsInfo/termTimes/index"
-// splits:
-import "./features/splits/splits/index"
-import "./features/splits/subSplits/index"
-import "./features/splits/padTimers/index"
-import "./features/splits/pdSplits/index"
