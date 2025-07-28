767cc77ab660cc61d46f910e3b8f26ba7743f8a7
undonecoffee <cameronwilcox101@gmail.com>
2025-07-28 00:34:15 -0500

    termtimes fixed

--- features/termsInfo/termTimes/index.js
+++ features/termsInfo/termTimes/index.js
@@ -1,11 +1,12 @@
 import settings from "../config/settings"
 import { termNames } from "./utils"
 
-// let openTime, inTerm
+let openTime, inTerm, terminalName
 register("packetReceived", (packet, event) => {
     const windowTitle = packet.func_179840_c().func_150254_d().removeFormatting()
+    inTerm = true
 
-    let terminalName = termNames.find(([_, regex]) => regex.test(windowTitle))?.[0]
+    terminalName = termNames.find(([_, regex]) => regex.test(windowTitle))?.[0]
     if (!terminalName) return console.log(`\n    not a term: ${windowTitle}\n`)
     openTime = Date.now()
     console.log(`\nWindowName: ${windowTitle}\n`)
@@ -23,6 +24,13 @@ register("packetReceived", (packet, event) => {
 //     this.currentItems.push([windowId, slot, itemStack, ctItem])
 // }).setFilteredClass(Java.type("net.minecraft.network.play.server.S2FPacketSetSlot"))
 
-// register("packetReceived", () => {
-//     ChatLib.chat(`${Prefix} &aTerminal &c${this.currentTitle} &acompleted in &c${(Date.now() - this.initialOpen) / 1000}s`)
-// }).setFilteredClass(Java.type("net.minecraft.network.play.server.S2EPacketCloseWindow"))
+register("packetReceived", () => {
+    if (!inTerm) return
+    console.log(`\n&3${terminalName} &acompleted in &c${(Date.now() - openTime) / 1000}s\n`)
+    inTerm = false
+}).setFilteredClass(Java.type("net.minecraft.network.play.server.S2EPacketCloseWindow"))
+
+register("packetSent", () => {
+    if (!inTerm) return
+    inTerm = false
+}).setFilteredClass(Java.type("net.minecraft.network.play.client.C0DPacketCloseWindow"))
